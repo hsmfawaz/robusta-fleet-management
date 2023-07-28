3faeaab69a158e9040bacf08aed9364c885e31f2
hsmfawaz2 <hsm.fawaz@gmail.com>
2023-07-28 15:05:55 +0300

    project migrations and models

---
+++ app/Models/Buses/BusSeat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Buses;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BusSeat extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_07_28_114500_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bus_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+        Schema::create('trip_stations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('station_id')->nullable()->constrained('stations')->nullOnDelete();
+            $table->foreignId('trip_id')->constrained()->cascadeOnDelete();
+            $table->unsignedInteger('station_order');
+            $table->boolean('current_station')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};

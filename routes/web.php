9e78b44be92485700d49c28b70419dcb81f0b3f8
hsmfawaz2 <hsm.fawaz@gmail.com>
2023-07-29 02:36:43 +0300

    refactoring dashboard controllers to new namespace for better grouping

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\BookingController;
-use App\Http\Controllers\Core\BusController;
-use App\Http\Controllers\Core\StationController;
-use App\Http\Controllers\Core\TripController;
-use App\Http\Controllers\DashboardHomeController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Dashboard\BookingController;
+use App\Http\Controllers\Dashboard\Core\BusController;
+use App\Http\Controllers\Dashboard\Core\StationController;
+use App\Http\Controllers\Dashboard\Core\TripController;
+use App\Http\Controllers\Dashboard\DashboardHomeController;
+use App\Http\Controllers\Dashboard\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'dashboard');
